9a7080bc00a43615be391121245f9987aba3f60f
kareem atef <kareem.atef@yahoo.com>
2023-11-17 02:33:38 +0200

    BugFix 0002_MinMax_Function.c

--- 0002_MinMax_Function.c
+++ 0002_MinMax_Function.c
@@ -1,28 +1,29 @@
 void miniMaxSum(int arr_count, int* arr) {
-    double min =0,max =0;
+    long long int  min =0,max =0;
 
 if((arr[0] + arr[1] + arr[2] + arr[3]) > max)
     max = (arr[0] + arr[1] + arr[2] + arr[3]);
-else if((arr[0] + arr[2] + arr[3] + arr[4]) > max)
+if((arr[0] + arr[2] + arr[3] + arr[4]) > max)
     max = (arr[0] + arr[2] + arr[3] + arr[4]);
-else if((arr[0] + arr[1] + arr[2] + arr[4]) >max)
+if((arr[0] + arr[1] + arr[2] + arr[4]) > max)
     max = (arr[0] + arr[1] + arr[2] + arr[4]);
-else if((arr[0] + arr[1] + arr[3] + arr[4]) > max)
+if((arr[0] + arr[1] + arr[3] + arr[4]) > max)
     max = (arr[0] + arr[1] + arr[3] + arr[4]);   
-else if((arr[1] + arr[2] + arr[3] + arr[4]) > max) 
+if((arr[1] + arr[2] + arr[3] + arr[4]) > max) 
     max = (arr[1] + arr[2] + arr[3] + arr[4]);
  
  min = max;
  
 if((arr[0] + arr[1] + arr[2] + arr[3]) < min)
-    max = (arr[0] + arr[1] + arr[2] + arr[3]);
-else if((arr[0] + arr[2] + arr[3] + arr[4]) < min)
-    max = (arr[0] + arr[2] + arr[3] + arr[4]);
-else if((arr[0] + arr[1] + arr[2] + arr[4]) < min)
-    max = (arr[0] + arr[1] + arr[2] + arr[4]);
-else if((arr[0] + arr[1] + arr[3] + arr[4]) < min)
-    max = (arr[0] + arr[1] + arr[3] + arr[4]);   
-else if((arr[1] + arr[2] + arr[3] + arr[4]) < min) 
-    max = (arr[1] + arr[2] + arr[3] + arr[4]);  
-    printf("%d %d",min,max);
+    min = (arr[0] + arr[1] + arr[2] + arr[3]);
+if((arr[0] + arr[2] + arr[3] + arr[4]) < min)
+    min = (arr[0] + arr[2] + arr[3] + arr[4]);
+if((arr[0] + arr[1] + arr[2] + arr[4]) < min)
+    min = (arr[0] + arr[1] + arr[2] + arr[4]);
+if((arr[0] + arr[1] + arr[3] + arr[4]) < min)
+    min = (arr[0] + arr[1] + arr[3] + arr[4]);   
+if((arr[1] + arr[2] + arr[3] + arr[4]) < min) 
+    min = (arr[1] + arr[2] + arr[3] + arr[4]);  
+
+    printf("%lld %lld",min,max);
 }
