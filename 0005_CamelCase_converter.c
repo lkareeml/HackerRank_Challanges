5bf002ac112bf5659d764215d1421fd2f1648657
kareem atef <kareem.atef@yahoo.com>
2023-11-18 17:35:24 +0200

    Update 0005_CamelCase_converter.c

--- 0005_CamelCase_converter.c
+++ 0005_CamelCase_converter.c
@@ -5,28 +5,52 @@
 #include <ctype.h>
 
 void converter(char * s, char* s_out, int n){
+    int S_Flag = 0;
+    int C_Flag = 0;
+    int M_Flag = 0;
     int j = 0;
-    for(int i = 4;i < n;i++){
-        if(s[0] == 'S'){
-            if(isupper(s[i])){
+    for(int i = 0;i < n;i++){
+        
+        if(s[i] == 'S' && s[i+1] == ';'){
+            i+=3;
+            S_Flag = 1;C_Flag = 0;
+            continue;
+        }
+        if(s[i] == 'C' && s[i+1] == ';'){
+            if(s[i+2] == 'M') 
+                M_Flag =1;
+            else 
+                M_Flag = 0;
+            C_Flag = 1;
+            S_Flag = 0;    
+            i+=3;
+            continue;
+        }
+        
+        if(S_Flag == 1 && C_Flag == 0){
+            if(isupper(s[i]) && s[i-1] != ';'){
                 s_out[j] = ' ';
                 printf("%c",s_out[j]);
                 j++;
                 s_out[j] = s[i] + 32;
-            }else{
-                if(s[i] == '\0' || s[i] == '(' || s[i] == ')'){
+            }
+            else if(isupper(s[i]) && s[i-1] == ';'){
+                s_out[j] = s[i] + 32;
+            }
+            else{
+            if(s[i] == '\0' || s[i] == '(' || s[i] == ')'){
                     break;
-                    }
+                }
                 s_out[j] = s[i];
             }
             printf("%c",s_out[j]);
             j++;
         }
-        if(s[0] == 'C'){
+        if(S_Flag == 0 && C_Flag == 1){
             if(s[i] != '\0' && s[i] != ' ' && s[i] != 32){
                 s_out[j] = s[i];
             }
-            else if(s[i] == 32){
+            else if(s[i] == ' '){
                 i++;
                 s_out[j] = (s[i] - 32);
             }
@@ -44,23 +68,23 @@ void converter(char * s, char* s_out, int n){
         
     }
 }
+
+
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT
-    // OPERATION :  S (split) or C (combine) 
-    // semi-colon 
-    /// M indicates method, C indicates class, and V indicates variable
-    S;M;plasticCup()            >   plastic cup
-    S;C;LargeSoftwareBook       >   large software book
-    S;V;pictureFrame            >   picture frame
-    C;V;mobile phone            >   mobilePhone
-    C;C;coffee machine          >   CoffeeMachine
-    C;M;white sheet of paper    >   whiteSheetOfPaper()
-    */
-    int n = 60;
+    // Enter your code here. Read input from STDIN. Print output to STDOUT
+    int n = 300;
     char s_input[n];
     char s_output[n];
-    fgets(s_input, n, stdin);
-    //scanf("%[^\n]s",&s_input1);
+    char temp;
+
+    for(int i =0; i < n ;i++){
+        s_input[i] = getchar();
+        if(s_input[i] < 10 || s_input[i] > 122) 
+        {s_input[i] = '\0';break;}
+        //if(s_input[i] > 11 && s_input[i] < 32)
+        //{s_input[i] = '\0';break;}
+        //if(s_input[i] == 10) break;
+    }
     converter(&s_input,&s_output,n);
     return 0;
 }
